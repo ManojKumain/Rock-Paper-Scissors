879ae364e7890a13d6736e3766d67f52dea2acbf
Manoj Chand <memanojkumain@gmail.com>
2025-09-22 06:15:56 +0530

    Step 1 Completed: Creating a function that generates computer choice

---
+++ script.js
@@ -0,0 +1,10 @@
+// Step 1: Create a function that generates computer choice
+function getComputerChoice() {
+    const choices = ['rock', "paper", 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+
