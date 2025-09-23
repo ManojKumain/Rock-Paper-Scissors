fcce2a1a97c8d34db2a86ebd825a36cb897de02f
Manoj Chand <memanojkumain@gmail.com>
2025-09-23 14:28:35 +0530

    rock paper scissors project completed

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-// Step 1: Create a function that generates computer choice
-function getComputerChoice() {
-    const choices = ['rock', "paper", 'scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3 + 1)  // or you can use possibleChoices.length
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+
 }
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lost!'
+    }
+    resultDisplay.innerHTML = result;
 
-
-// Step 2: Writing a function to get human choice
-function getHumanChoice() {
-    const choose = prompt("Enter your Choice");
-    return choose;
 }
-
-const humanChoice = getHumanChoice();
-console.log(humanChoice);
